149e4f29bd4533ee7dc8e0f6237588ad8ee1626e
Michael Soucek <michaelsoucek@Michaels-MacBook-Air.local>
2026-03-24 08:39:18 -0400

    Add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import Home from './pages/index';
 
 const App: React.FC = () => {
 	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Home />} />
-			</Routes>
-		</Router>
+		<>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</Router>
+			<Analytics />
+		</>
 	);
 };
 
